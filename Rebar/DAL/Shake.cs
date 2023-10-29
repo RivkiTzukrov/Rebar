5bc5c56e08fd1ddb30b8849f906d71a1736bf70e
Rivka Tzukrov <ri0583273093@gmail.com>
2023-10-29 20:10:59 +0200

    DAL added to project

---
+++ Rebar/DAL/Shake.cs
@@ -0,0 +1,11 @@
+﻿namespace DAL;
+
+public class Shake
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double PriceL { get; set; }
+    public double PriceM { get; set; }
+    public double PriceS { get; set; }
+}
