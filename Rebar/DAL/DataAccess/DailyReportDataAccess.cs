e623387c0501defab51e9d26d77d3bd80f231d51
Rivka Tzukrov <ri0583273093@gmail.com>
2023-10-31 14:19:05 +0200

    DailyReport added to DAL

---
+++ Rebar/DAL/DataAccess/DailyReportDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Models;
+
+namespace DAL.DataAccess;
+
+public class DailyReportDataAccess : DataAccess
+{
+    private const string DailyReportCollection = "dailyReport";
+
+    public Task CreateDailyReport(DailyReportModel dailyReport)
+    {
+        var dailyReportCollection = ConnectToMongo<DailyReportModel>(DailyReportCollection);
+        return dailyReportCollection.InsertOneAsync(dailyReport);
+    }
+}
